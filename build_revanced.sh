e0f5f1a0d2ead34e756409cb6bc7f6918fc7b828
Tititvoot Pangrit <tanlnwtitivoot@gmail.com>
2022-06-29 19:01:38 +0700

    remove patches fix bug and add more patches
    remove patches fix comments bug and add old-quality-layout

--- build_revanced.sh
+++ build_revanced.sh
@@ -61,7 +61,7 @@ echo "************************************"
 
 mkdir -p build
 # Obtained from: revanced-patches-1.9.1
-available_patches="-e amoled -e custom-branding -e old-quality-layout -e hide-cast-button -e tasteBuilder-remover"
+available_patches="-e amoled -e custom-branding -e disable-fullscreen-panels -e hide-cast-button -e tasteBuilder-remover"
 
 if [ -f "com.google.android.youtube.apk" ]
 then
